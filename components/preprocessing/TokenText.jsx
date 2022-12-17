e6813c021e78f20ea46e6ac8107c71be2a90c9b7
livingdolls <nanangyorozuya@gmail.com>
2022-12-17 17:35:09 +0700

    readme

--- components/preprocessing/TokenText.jsx
+++ components/preprocessing/TokenText.jsx
@@ -1,18 +1,27 @@
 import { Box } from "@mui/material";
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
 import HeadCell from "../Tabel/HeadCell";
 import MainTabel from "../Tabel/MainTabel";
 import TabelHead from "../Tabel/TabelHead";
 
-const TokenText = ({data}) => {
-    return(
-        <Box sx={{mt:3, width:'90%', marginLeft:'auto', marginRight:'auto'}}>
+const TokenText = ({ data }) => {
+    return (
+        <Box
+            sx={{
+                mt: 3,
+                width: "90%",
+                marginLeft: "auto",
+                marginRight: "auto",
+            }}
+        >
             <MainTabel>
                 <TabelHead>
                     <TableRow>
-                        <HeadCell align="left" size='small'>No</HeadCell>
+                        <HeadCell align="left" size="small">
+                            No
+                        </HeadCell>
                         <HeadCell>Jurnal id</HeadCell>
                         <HeadCell>Token</HeadCell>
                         <HeadCell>Kode Jurnal</HeadCell>
@@ -20,21 +29,22 @@ const TokenText = ({data}) => {
                 </TabelHead>
 
                 <TableBody>
-                    {data.map((d,index) => {
+                    {data.map((d, index) => {
                         return (
                             <TableRow>
-                                <TableCell align="left" size='small'>{ index }</TableCell>
-                                <TableCell>{ d.jurnal_id }</TableCell>
-                                <TableCell>{ d.token }</TableCell>
-                                <TableCell>{ d.kd_jurnal }</TableCell>
+                                <TableCell align="left" size="small">
+                                    {index}
+                                </TableCell>
+                                <TableCell>{d.jurnal_id}</TableCell>
+                                <TableCell>{d.token}</TableCell>
+                                <TableCell>{d.kd_jurnal}</TableCell>
                             </TableRow>
-                        )
+                        );
                     })}
-
                 </TableBody>
-        </MainTabel>
-    </Box>
-    )
-}
+            </MainTabel>
+        </Box>
+    );
+};
 
-export default TokenText
+export default TokenText;
